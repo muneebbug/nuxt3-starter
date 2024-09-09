7a7911f2c4f137cccc823b7ef88f510fe98ac616
Muneeb <freelancermuneeb@gmail.com>
2024-09-09 10:25:37 +0500

    refactor ♻️: callback page implemented in shad-cn

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -1,31 +1,38 @@
 <template>
-  <div>
-    <n-result
+  <div class="flex justify-center items-center">
+    <div
       v-if="$route.query.error"
-      status="error"
-      title="Failed to login"
-      :description="$route.query.error.toString()"
     >
-      <template #footer>
-        <nuxt-link
+      <div class="text-center">
+        <Icon
+          name="oui:cross-in-circle-filled"
+          class="text-red-500 mb-2"
+          size="64px"
+        />
+        <p class="text-2xl font-semibold mb-1">
+          Failed to login
+        </p>
+        <p class="text-gray-500 mb-4">
+          {{ $route.query.error.toString() }}
+        </p>
+        <Button
           to="/auth/login"
-          class="no-underline"
         >
-          <n-button type="primary">
-            Go back to login
-          </n-button>
-        </nuxt-link>
-      </template>
-    </n-result>
+          Go back to login
+        </Button>
+      </div>
+    </div>
 
-    <n-spin
+    <Loader2
       v-else
-      size="large"
+      class="w-12 h-12 animate-spin"
     />
   </div>
 </template>
 
 <script setup>
+import { Loader2 } from 'lucide-vue-next'
+
 definePageMeta({
   layout: 'auth',
   colorMode: 'light',
