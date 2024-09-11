da2c3753814484edbe17db86601b81edac1797d7
Muneeb <freelancermuneeb@gmail.com>
2024-09-11 10:15:55 +0500

    refactor ♻️: use trpc mutation to update user profile

--- components/Account/Profile.vue
+++ components/Account/Profile.vue
@@ -149,13 +149,15 @@ const onSubmit = handleSubmit(async (values) => {
     values.picture = url
   }
 
-  await useNuxtApp().$auth.fetch('/api/user', {
-    method: 'patch',
-    body: {
-      name: values.name,
-      picture: values.picture,
-    },
-  })
+  await useNuxtApp().$client.user.update.mutate(values)
+
+  // await useNuxtApp().$auth.fetch('/api/user', {
+  //   method: 'patch',
+  //   body: {
+  //     name: values.name,
+  //     picture: values.picture,
+  //   },
+  // })
 
   await fetchUser()
   setValues({
--- server/trpc/router.ts
+++ server/trpc/router.ts
@@ -1,8 +1,10 @@
 import { router } from './trpc'
 import { incrementRouter } from './routers/increment'
+import { userRouter } from './routers/user'
 
 export const appRouter = router({
   increment: incrementRouter,
+  user: userRouter,
 })
 
 // export type definition of API
